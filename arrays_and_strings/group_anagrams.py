e3fcacda99398c22bc6dc8eb8fdae7d0a12cb4e1
karthik <karthik4636@gmail.com>
2018-10-22 11:55:04 -0400

    group anagrams

---
+++ arrays_and_strings/group_anagrams.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def get_dict(self,str):
+        a ={}
+        for i in str:
+            if i not in a:
+                a[i] = 1
+            else:
+                a[i]+=1
+        return a
+
+
+
+
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        b = {}
+        for str in strs:
+            sig = self.get_dict(str)
+            sig = tuple(sorted(sig.items()))
+            if sig not in b:
+                b[sig]=[str]
+            else:
+                b[sig].append(str)
+
+        return list(b.values())
+
+
+s = Solution()
+a=s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+b=1
