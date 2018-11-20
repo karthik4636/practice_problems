a0249770c3638c386d272799e0965dbc55d57b58
karthik <karthik4636@gmail.com>
2018-11-19 19:15:50 -0500

    bucket sort

--- arrays_and_strings/top_k_frequent.py
+++ arrays_and_strings/top_k_frequent.py
@@ -1,4 +1,4 @@
-#https://leetcode.com/problems/top-k-frequent-elements/
+# https://leetcode.com/problems/top-k-frequent-elements/
 class Solution:
     def topKFrequent(self, nums, k):
         """
@@ -12,22 +12,46 @@ class Solution:
             if i not in freq:
                 freq[i] = 1
             else:
-                freq[i] +=1
+                freq[i] += 1
 
         d1 = sorted(freq.items(), key=lambda x: x[1], reverse=True)
         b = []
         for key, value in d1:
-            if k <=0:
+            if k <= 0:
                 break
             b.append(key)
-            k-=1
+            k -= 1
         return sorted(b)
 
+    def bucket_sort(self, nums):
+        hashmap = {}
 
+        # buckets
+        buckets = [[] for i in range(len(nums) + 1)]
 
+        # record the num of instances of occurrence for each number
+        for num in nums:
+            if num not in hashmap:
+                hashmap[num] = 1
+            else:
+                hashmap[num] += 1
 
+        # add hashmap entries according to frequencies to freq-array
+        # index = frequency
+        for number, frequency in hashmap.items():
+            buckets[frequency].append(number)
+
+        res = []
+
+        i = len(buckets) - 1
+
+        while i > 0 and len(res) < k:
+            res.extend(buckets[i])
+            i -= 1
+
+        return res
 
 
 s = Solution()
-s.topKFrequent( nums = [4,1,-1,2,-1,2,3], k = 2)
+s.topKFrequent(nums=[4, 1, -1, 2, -1, 2, 3], k=2)
 
