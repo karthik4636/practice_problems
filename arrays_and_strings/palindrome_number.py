dba1b2532b9a35c541d772629cbad5ffd3263fec
karthik <karthik4636@gmail.com>
2018-12-06 19:55:20 -0500

    palindrome number

---
+++ arrays_and_strings/palindrome_number.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    #convert to string and do it
+    def isPalindrome_convert_to_string(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        pass
+
+    # without conversion to string
+    def is_palindrome(self):
+        pass
+
+
+
+
