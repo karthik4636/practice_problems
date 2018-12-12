1d9d913ed787c8dc2b7e3f4babe98671f6d99085
karthik <karthik4636@gmail.com>
2018-12-11 19:18:46 -0500

    recurive approach

---
+++ trees/validate_BST.py
@@ -0,0 +1,24 @@
+class Solution(object):
+
+    def is_valid_helper(self, root, min, max):
+        if root is None:
+            return True
+
+        if root.val <= min or root.val >= max:
+            return False
+
+
+        return self.is_valid_helper(root.left,min, root.val) and \
+          self.is_valid_helper(
+            root.right,root.val,max)
+
+
+
+
+
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.is_valid_helper(root,float("-inf"), float("inf"))
