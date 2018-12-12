f96c398436b2243f406ee751e985792ce64b5b99
karthik <karthik4636@gmail.com>
2018-12-11 19:20:17 -0500

    validate bst

--- trees/validate_BST.py
+++ trees/validate_BST.py
@@ -1,3 +1,5 @@
+#https://leetcode.com/problems/validate-binary-search-tree/
+
 class Solution(object):
 
     def is_valid_helper(self, root, min, max):
@@ -7,18 +9,13 @@ class Solution(object):
         if root.val <= min or root.val >= max:
             return False
 
-
-        return self.is_valid_helper(root.left,min, root.val) and \
-          self.is_valid_helper(
-            root.right,root.val,max)
-
-
-
-
+        return self.is_valid_helper(root.left, min, root.val) and \
+               self.is_valid_helper(
+                   root.right, root.val, max)
 
     def isValidBST(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        return self.is_valid_helper(root,float("-inf"), float("inf"))
+        return self.is_valid_helper(root, float("-inf"), float("inf"))
