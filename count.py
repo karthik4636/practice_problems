f78f3e62f728dbf6d131c6cbc5cc6f5c448478f3
karthik <karthik4636@gmail.com>
2018-12-18 18:16:33 -0500

    count includes sorting

--- count.py
+++ count.py
@@ -1,7 +1,7 @@
 import os
 count = 0
 path, dirs, files = next(os.walk("/home/karthik/Desktop/interview_prep/practice_problems"))
-for dir in ['trees','linked_list', 'arrays_and_strings']:
+for dir in ['trees','linked_list', 'arrays_and_strings',"sorting"]:
     path, dirs, files = next(
         os.walk(dir))
     print(dir, len(files))
