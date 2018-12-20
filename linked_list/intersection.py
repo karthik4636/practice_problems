3fd404598c282f361436f38a75222d98fd03536b
karthik <karthik4636@gmail.com>
2018-12-20 16:04:01 -0500

    intersection of linked lists

---
+++ linked_list/intersection.py
@@ -0,0 +1,33 @@
+#https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#Note : if there is no intersection both pointers point to None, else they
+# point to the intersection
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if headA is None or headB is None:
+            return None
+
+        p1, p2 = headA, headB
+
+        while p1 is not p2:
+            if p1 is not None:
+                p1 = p1.next
+            else:
+                p1 = headB
+
+            if p2 is not None:
+                p2 = p2.next
+            else:
+                p2 = headA
+        return p1
