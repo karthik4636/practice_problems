12a00a399e9dda6d02de60dd369aa91fc2454e1d
karthik <karthik4636@gmail.com>
2018-12-19 21:24:12 -0500

    merge sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,30 +1,44 @@
-#Note we are using same array arr and not creating new arrays for merging
+#https://www.youtube.com/watch?v=Nso25TkBsYI
+# there are log n steps and each element is gone through n times. SO for n
+# elements run time is o(n log n)
 
 
-def merge_two_halves(arr,left, right):
+def merge_two_halves(arr, left, right):
     left_pointer = 0
     right_pointer = 0
     index = 0
-    pass
+    while left_pointer < len(left) and right_pointer < len(right):
+        if left[left_pointer] < right[right_pointer]:
+            arr[index] = left[left_pointer]
+            left_pointer += 1
+        else:
+            arr[index] = right[right_pointer]
+            right_pointer += 1
+        index += 1
+    while left_pointer < len(left):
+        arr[index] = left[left_pointer]
+        index += 1
+        left_pointer += 1
 
-
-
-
-
-
-
-
+    while right_pointer < len(right):
+        arr[index] = right[right_pointer]
+        right_pointer += 1
+        index += 1
+    return arr
 
 
 def merge_sort(arr):
-    if len(arr)<=1:
-        return
-    mid = len(arr)//2
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
     left = arr[:mid]
     right = arr[mid:]
     merge_sort(left)
     merge_sort(right)
-    merge_two_halves(arr,left,right)
+    arr = merge_two_halves(arr, left, right)
+    return arr
 
 
 
+a=merge_sort([1,6,5,-10,100,4,5,90,97,0,8,67,59,-89,-69])
+print(a)
