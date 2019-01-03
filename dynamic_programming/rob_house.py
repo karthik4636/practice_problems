bb93bfc2f2a93aaaa96d489c9580e45338187d66
karthik <karthik4636@gmail.com>
2019-01-03 18:11:49 -0500

    space complexity reduced

--- dynamic_programming/rob_house.py
+++ dynamic_programming/rob_house.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def rob(self, nums):
         """
+        space complexity here is O(n)
         :type nums: List[int]
         :rtype: int
         """
@@ -24,7 +25,29 @@ class Solution(object):
         return max_till_now[-1]
 
 
+    def rob_2(self,nums):
+        """
+        space complexity o(1)
+        :param nums:
+        :return:
+        """
+        size = len(nums)
+        if size == 0:
+            return 0
+        if size == 1:
+            return nums[0]
+        if size == 2:
+            return max(nums[0], nums[1])
 
+        a = nums[0]
+        b = max(nums[0], nums[1])
+        max_till_now = None
+        for i in range(2, size):
+            max_till_now = max(b, nums[i] + a)
+            a = b
+            b = max_till_now
+
+        return max_till_now
 
 
 s = Solution()
