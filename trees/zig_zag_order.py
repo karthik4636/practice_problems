f62621c10ab777535fcc9a76c9bea8ae9d32ff94
karthik <karthik4636@gmail.com>
2019-01-14 18:39:51 -0500

    zig zag

---
+++ trees/zig_zag_order.py
@@ -0,0 +1,22 @@
+class Solution:
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+
+        if not root:
+            return []
+
+        ans, cur, temp, flag = [], [root], [], 1
+        while cur:
+            ans.append(list(x.val for x in cur[::flag]))
+            for node in cur:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            cur, temp = temp, []
+            flag *= -1
+
+        return ans
