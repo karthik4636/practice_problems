37e3b99512fdebf6324af1cdb66a42591c721ac2
karthik <karthik.suresh@siriusxm.com>
2019-10-06 19:07:07 -0400

    palindromic substrings

---
+++ arrays_and_strings/palindrome_substring.py
@@ -0,0 +1,24 @@
+#https://helloacm.com/algorithms-to-count-the-number-of-palindromic-substrings/
+class Solution(object):
+    def countSubstrings(self, S):
+        count = 0
+        for i in range(len(S)):
+            j=i
+            k=i
+            while j>=0 and k<=len(S)-1 and S[j]==S[k]:
+                j-=1
+                k+=1
+                count+=1
+            j=i
+            k=i+1
+            while (j >= 0 and k <= len(S) - 1 and S[j] == S[k]):
+                j -= 1
+                k += 1
+                count += 1
+        return count
+
+
+
+
+s = Solution()
+s.countSubstrings("abba")
