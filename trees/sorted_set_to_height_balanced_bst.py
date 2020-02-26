ac67134f1fbb4aa789d655f0e97e756406c80756
karthik <karthik.suresh@siriusxm.com>
2020-02-26 11:54:32 -0500

    sorted to bst

---
+++ trees/sorted_set_to_height_balanced_bst.py
@@ -0,0 +1,34 @@
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+
+
+
+    def sortedArrayToBST(self, nums):
+        if nums:
+            size = len(nums)
+            if size == 1:
+                root = TreeNode(nums[0])
+                return root
+            else:
+                mid = size//2
+                root = TreeNode(nums[mid])
+                root.left=self.sortedArrayToBST(nums[:mid])
+                root.right = self.sortedArrayToBST(nums[mid+1:])
+                return root
+
+
+
+
+
+
+s = Solution()
+a = [-10,-3,0,5,9]
+b = [-3,0]
+s.sortedArrayToBST(a)
+
