eb06a378aaa71c943270457b48819b5b978a4635
karthik <karthik.suresh@siriusxm.com>
2020-05-20 23:13:53 -0400

    TreeNode

--- Temp/sorted_list_to_bst.py
+++ Temp/sorted_list_to_bst.py
@@ -1,4 +1,11 @@
 #https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def sortedListToBST(self, head):
         curr = head
